94b7c9b3dafc89347672b3564505563fadf3e2bb
Dwiky Alan <dwikyalanadi@gmail.com>
2019-02-27 21:06:55 +0700

    recycler-view-tugas

---
+++ app/src/main/java/com/example/win10/recyclerviewtugas/TugasRecyclerview.java
@@ -0,0 +1,13 @@
+package com.example.win10.recyclerviewtugas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TugasRecyclerview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tugas_recyclerview);
+    }
+}
